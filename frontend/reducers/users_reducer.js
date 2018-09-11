ea159a6e43dfe9c6cd0e652edc24186f0428faca
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2018-09-11 15:21:06 -0400

    Add RECEIVE_CURRENT_USER case in userReducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,5 +1,14 @@
+import { merge } from "lodash";
+
+import * as ActionConstants from "../constants/action_constants";
+
 const usersReducer = (state = {}, action) => {
+  const newState = merge({}, state);
+
   switch (action.type) {
+    case ActionConstants.RECEIVE_CURRENT_USER:
+      newState[action.currentUser.id] = action.currentUser;
+      return newState;
     default:
       return state;
   }
