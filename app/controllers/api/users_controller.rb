ea14bf960cc1fb68916b8f698b2459781242d26a
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2018-09-11 13:44:31 -0400

    Add api User views and UsersController

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :img_url)
+  end
+end
