485716b1ffbc11b99760f61755ebacd939021019
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-11 15:35:02 -0400

    Add Root and App components

--- frontend/asana.jsx
+++ frontend/asana.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 
 import configureStore from "./store/store";
 
+import Root from "./components/root";
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
 
-  ReactDOM.render(<h1>Asana</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
