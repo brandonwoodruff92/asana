d6f842fb5f679e354d23a59080f108e2e233e2dc
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2018-09-11 15:29:04 -0400

    Clean up entry file and sessionErrorsReducer

--- frontend/asana.jsx
+++ frontend/asana.jsx
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom";
 
 import configureStore from "./store/store";
 
-import SessionApiUtil from "./util/session_api_util";
-
-import { login, logout } from "./actions/session_actions";
-
 document.addEventListener("DOMContentLoaded", () => {
-  window.SessionApiUtil = SessionApiUtil;
   const root = document.getElementById("root");
   const store = configureStore();
-  window.dispatch = store.dispatch;
-  window.login = login;
-  window.logout = logout;
+
   ReactDOM.render(<h1>Asana</h1>, root);
 });
