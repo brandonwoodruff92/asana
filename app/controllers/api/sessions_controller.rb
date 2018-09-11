cd8d6d1ff3c337f003f650c6a195d7f9386ef76b
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-11 12:01:39 -0400

    Add SessionsController and integrate backend auth pattern

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["The username or password is not correct."], status: 401
+    end
+  end
+
+  def destroy
+    logout!
+    render json: {}
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911152634) do
+ActiveRecord::Schema.define(version: 20180911155706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "users", null: false
+    t.string "name", null: false
     t.string "email", null: false
     t.string "img_url"
     t.string "password_digest", null: false
