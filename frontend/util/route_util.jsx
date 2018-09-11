6ff821f5b4adf2e7003d796996d9a1f5010cf81a
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-11 16:47:04 -0400

    Push for initial Heroku push

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => {
+  function toRender(props) {
+    if (loggedIn) {
+      
+    }
+  }
+}
