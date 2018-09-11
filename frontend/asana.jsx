08f8415db44defe651d9d92ed220bc94940f9535
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-11 15:45:45 -0400

    Add currentUser persistence after page refreshes

--- frontend/asana.jsx
+++ frontend/asana.jsx
@@ -5,9 +5,35 @@ import configureStore from "./store/store";
 
 import Root from "./components/root";
 
+//TESTING IMPORTS//
+import { login, logout } from "./actions/session_actions";
+//TESTING IMPORTS//
+
 document.addEventListener("DOMContentLoaded", () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: {
+        id: window.currentUser.id
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   const root = document.getElementById("root");
-  const store = configureStore();
+
+  //TESTING//
+  window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  window.login = login;
+  window.logout = logout;
+  //TESTING//
 
   ReactDOM.render(<Root store={ store }/>, root);
 });
