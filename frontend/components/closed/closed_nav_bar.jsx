300b56912bded67f6550c32119794f59b20997d5
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-12 13:50:12 -0400

    Prepare for heroku push

--- frontend/components/closed/closed_nav_bar.jsx
+++ frontend/components/closed/closed_nav_bar.jsx
@@ -4,9 +4,14 @@ import { NavLink } from "react-router-dom";
 
 const ClosedNavBar = (props) => {
   return (
-    <div>
-      <NavLink to={ RouteConstants.LOGIN }>Log In</NavLink>
-      <NavLink to={ RouteConstants.SIGNUP }>Start Free Trial</NavLink>
+    <div className="nav-bar">
+      <div className="left-nav">
+        <NavLink id="home-btn-logo" to={ RouteConstants.CLOSED_ROOT }></NavLink>
+      </div>
+      <div className="right-nav">
+        <NavLink id="login" to={ RouteConstants.LOGIN }>Log In</NavLink>
+        <NavLink id="signup" to={ RouteConstants.SIGNUP }>Start Free Trial</NavLink>
+      </div>
     </div>
   );
 };
