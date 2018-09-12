8ed6cd61c0114653766d0d9bded144ff49014660
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-12 16:19:46 -0400

    Add closed nav bar styling

--- frontend/components/closed/closed_nav_bar.jsx
+++ frontend/components/closed/closed_nav_bar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import * as RouteConstants from "../../constants/route_constants";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const ClosedNavBar = (props) => {
   return (
     <div className="nav-bar">
-      <div className="left-nav">
+      <div className="nav-bar-left">
         <NavLink id="home-btn-logo" to={ RouteConstants.CLOSED_ROOT }></NavLink>
       </div>
-      <div className="right-nav">
-        <NavLink id="login" to={ RouteConstants.LOGIN }>Log In</NavLink>
-        <NavLink id="signup" to={ RouteConstants.SIGNUP }>Start Free Trial</NavLink>
+      <div className="nav-bar-right">
+        <nav className="nav-bar-item nav-tabs">
+          <NavLink className="nav-tab-item" id="login" to={ RouteConstants.LOGIN }>Log In</NavLink>
+        </nav>
+        <NavLink id="signup" className="nav-bar-item" to={ RouteConstants.SIGNUP }>Start Free Trial</NavLink>
       </div>
     </div>
   );
