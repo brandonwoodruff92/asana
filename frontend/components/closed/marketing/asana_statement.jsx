f4735c91cf2cca05b2265df73ff9d7717e304d60
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-12 17:59:15 -0400

    Initial content styling

--- frontend/components/closed/marketing/asana_statement.jsx
+++ frontend/components/closed/marketing/asana_statement.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const AsanaStatement = () => {
   return (
-    <div>
-      <h3>Take the work out of getting work done</h3>
+    <div className="statement-content">
+      <h3 className="asana-moto">Take the work out of getting work done</h3>
 
-      <h5>Asana helps you coordinate all the work your team does together.
+      <h5 className="asana-statement">Asana helps you coordinate all the work your team does together.
         So everyone knows what needs to get done,
         who’s responsible for doing it, and when it’s due.</h5>
     </div>
