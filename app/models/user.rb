f917563b1acd2750a06435f50f486c7ae90150e8
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-13 17:50:14 -0400

    Add user email server validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   after_initialize :ensure_session_token
 
+  validates_with EmailValidator
+
   validates :name, :email, :password_digest, :session_token, presence: true
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
