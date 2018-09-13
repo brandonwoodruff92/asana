44f999b15a2446d66fbb83ba73b25a505b750c88
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-13 09:43:03 -0400

    Initial picture component

---
+++ frontend/components/closed/marketing/landing_picture.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPicture = () => {
+  return (
+    <div className="landing-picture-container">
+      <img src="https://luna1.co/bbbf3e.png" />
+    </div>
+  );
+};
+
+export default LandingPicture;
--- frontend/components/closed/marketing/marketing_content.jsx
+++ frontend/components/closed/marketing/marketing_content.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import AsanaStatement from "./asana_statement";
+import LandingPicture from "./landing_picture";
 
 const MarketingContent = (props) => {
   return (
     <div className="marketing-content">
       <AsanaStatement />
+      <LandingPicture />
     </div>
   );
 };
