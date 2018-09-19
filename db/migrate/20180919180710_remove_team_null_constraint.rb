4167e42b7a1752c3b459117dd549cdf91eb3b050
Brandon Woodruff <brandonwoodruff92@gmail.com>
2018-09-19 14:22:32 -0400

    Prep heroku push

--- db/migrate/20180919180710_remove_team_null_constraint.rb
+++ db/migrate/20180919180710_remove_team_null_constraint.rb
@@ -1,5 +1,5 @@
 class RemoveTeamNullConstraint < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :team_id, :integer, null: false
+    change_column :users, :team_id, :integer, null: true
   end
 end
